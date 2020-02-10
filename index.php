167761ed52046626c67dfa0ab764052bba324dc6
uxdarien <darien@moratoo.com>
2020-02-10 16:29:59 -0500

    Servidores

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 	<title>Linea del Tiempo • Historia Bíblica</title>
-	<base href='https://testiar.com/t/a/study/'>
+	<base href='/'>
 	<meta charset="utf-8">
 	<meta name="description" content="">
 	<link rel="icon" type="image/png" href="css/img/favicon.png" />
@@ -964,7 +964,9 @@
 
 
 
+	// Para Agregar Eventos de Angel
 
+	var json_data = <?php require '/events.php'; ?>
 
 
 
