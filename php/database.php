4b7d6f95aa7243eefd641bcd7e9675e2cbfceb8f
uxdarien <darien@moratoo.com>
2020-02-09 20:54:49 -0500

    Correccion De Datos y base de datos

---
+++ php/database.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'const.php';
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+
+echo "Connected";
+
+// Storing in Array
+if ($conn->connect_error) {
+  die('Database Error:' . $conn->connect_error);
+}
