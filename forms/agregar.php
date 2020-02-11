7761bcffbd1f644fc77cc7de93a75d310af5ad0b
uxdarien <darien@moratoo.com>
2020-02-11 16:24:21 -0500

    Archivo Corriendo

--- forms/agregar.php
+++ forms/agregar.php
@@ -31,7 +31,7 @@
  <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
- <link href="https://preview.webpixels.io/purpose-application-ui-kit-v1.0.0/assets/css/purpose.css" rel="stylesheet">
+ <link href="https://demo.webpixels.io/purpose-application-ui-kit-v1.0.0/assets/css/purpose.css" rel="stylesheet">
  <!-- <link href="style.css" rel="stylesheet"> -->
 
  </head>
