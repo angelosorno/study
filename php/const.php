1f663d2531e435d8aea986276b8c5bf0e04804f3
Webmaster <web@fundacionmarialuisa.org>
2020-02-11 21:20:09 -0500

    Update DB

--- php/const.php
+++ php/const.php
@@ -1,6 +1,6 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'b70r8s6n_timelin');
-define('DB_PASS', 'ui)%Ut@=2_Y8');
-define('DB_NAME', 'b70r8s6n_timeline');
+define('DB_USER', 'cemp_study');
+define('DB_PASS', '0LExOf5b25ST[C?nIm');
+define('DB_NAME', 'cemp_study');
