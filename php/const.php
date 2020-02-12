7541e29fd8318d04c4fef7a1255ba5093cf59f54
Webmaster <web@fundacionmarialuisa.org>
2020-02-12 00:50:03 -0500

    Change IP Database

--- php/const.php
+++ php/const.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', '198.72.112.185');
 define('DB_USER', 'cemp_study');
 define('DB_PASS', '0LExOf5b25ST[C?nIm');
 define('DB_NAME', 'cemp_study');
