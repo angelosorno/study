36955cf5cc13e4e2247fd15664487843c1c243ef
Angel Osorno <angel@moratoo.com>
2020-05-22 19:26:22 -0500

    Update index Error 500

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
-<?php  ?>
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Linea del Tiempo • Historia Bíblica</title>
-	<?php   require  ('url_base.php'); ?>
 	<meta charset="utf-8">
 	<meta name="description" content="">
 	<link rel="icon" type="image/png" href="css/img/favicon.png" />
