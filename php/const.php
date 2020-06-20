ee01a5600cfadcdae32d845fb1fbd44f6d0f71be
Darien Osorno <darienosorno@Dariens-MacBook-Pro.local>
2020-06-20 00:59:39 -0500

    Cambio servidor Darien

--- php/const.php
+++ php/const.php
@@ -7,7 +7,7 @@
 
 // DB SERVER DARIEN
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', '50.116.94.166');
 define('DB_USER', 'b70r8s6n_timelin');
 define('DB_PASS', 'ui)%Ut@=2_Y8');
 define('DB_NAME', 'b70r8s6n_timeline');
