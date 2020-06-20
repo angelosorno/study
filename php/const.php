d18a7b1ec08794f1ed2226cc220be42363566c7a
Angel Osorno <angel@moratoo.com>
2020-06-20 01:12:43 -0500

    Change DB to Server Hulk

--- php/const.php
+++ php/const.php
@@ -1,13 +1,13 @@
 <?php
 
-// define('DB_HOST', '198.72.112.185');
-// define('DB_USER', 'cemp_study');
-// define('DB_PASS', '0LExOf5b25ST[C?nIm');
-// define('DB_NAME', 'cemp_study');
+define('DB_HOST', '198.72.112.185');
+define('DB_USER', 'cemp_study');
+define('DB_PASS', '0LExOf5b25ST[C?nIm');
+define('DB_NAME', 'cemp_study');
 
 // DB SERVER DARIEN
 
-define('DB_HOST', '50.116.94.166');
-define('DB_USER', 'b70r8s6n_timelin');
-define('DB_PASS', 'ui)%Ut@=2_Y8');
-define('DB_NAME', 'b70r8s6n_timeline');
+// define('DB_HOST', '50.116.94.166');
+// define('DB_USER', 'b70r8s6n_timelin');
+// define('DB_PASS', 'ui)%Ut@=2_Y8');
+// define('DB_NAME', 'b70r8s6n_timeline');
