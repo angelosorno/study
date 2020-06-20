20fdc435a71bd22b010050959eda9b3a3f8bebf9
Darien Osorno <darienosorno@Dariens-MacBook-Pro.local>
2020-06-20 00:27:23 -0500

    Para que funcione con datos del JSON Original en ES

--- php/const.php
+++ php/const.php
@@ -1,13 +1,13 @@
 <?php
 
-define('DB_HOST', '198.72.112.185');
-define('DB_USER', 'cemp_study');
-define('DB_PASS', '0LExOf5b25ST[C?nIm');
-define('DB_NAME', 'cemp_study');
+// define('DB_HOST', '198.72.112.185');
+// define('DB_USER', 'cemp_study');
+// define('DB_PASS', '0LExOf5b25ST[C?nIm');
+// define('DB_NAME', 'cemp_study');
 
 // DB SERVER DARIEN
 
-// define('DB_HOST', 'localhost');
-// define('DB_USER', 'b70r8s6n_timelin');
-// define('DB_PASS', 'ui)%Ut@=2_Y8');
-// define('DB_NAME', 'b70r8s6n_timeline');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'b70r8s6n_timelin');
+define('DB_PASS', 'ui)%Ut@=2_Y8');
+define('DB_NAME', 'b70r8s6n_timeline');
