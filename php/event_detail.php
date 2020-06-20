d6b7e4514e38211b95ad539390761566423729e6
Darien Osorno <darienosorno@Dariens-MacBook-Pro.local>
2020-06-20 13:16:37 -0500

    Configuracion para que funcione con JSON y ignore las tildes de las DB

--- php/event_detail.php
+++ php/event_detail.php
@@ -2,12 +2,20 @@
 
 $slug = $_GET['slug'];
 
-$result_global = mysqli_query($conn, "SELECT * FROM personajes_original_test WHERE slug = '".$slug."'");
+$result_global = mysqli_query($conn, "SELECT * FROM personajes_original_es WHERE slug = '".$slug."'");
 $data_global = mysqli_fetch_assoc($result_global);
 // $data_global = array();
 // while ($row_global = mysqli_fetch_assoc($result_global)){
 //   $data_global[] = $row_global;
 // };
+
+// function my_apply_htmlentities(&$value)
+// {
+//     $value = htmlentities($value);
+// }
+// array_walk_recursive($data_global, 'my_apply_htmlentities');
+// var_dump($data_global);
+
 
 // Json Encode Data
 $json_global = json_encode($data_global);
